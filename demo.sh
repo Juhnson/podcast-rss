f82c7614a7cab404f99d89711b22424495742699
Juhnson <Juhnson.Guo@gmail.com>
2018-02-22 11:18:44 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ALBUMID="12825974"
-RSSFILE="2018sfcy.xml"
-# export PBUUID=""
+RSSFILE="PAF4.xml"
+export PBUUID="e42d7f6a-5541-44f3-abbe-fc93bf3fac7f"
 ./ximalaya_linux_amd64 $ALBUMID > $RSSFILE
 ls
 curl -X PUT -F c=@$RSSFILE https://ptpb.pw/$PBUUID
